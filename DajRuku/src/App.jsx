f3b51988969578c3e7ed8da9636b9a46c4756564
Marta_KO98 <martakokic98@gmail.com>
2026-05-16 15:01:08 +0200

    Fixed merge conflict

--- DajRuku/src/App.jsx
+++ DajRuku/src/App.jsx
@@ -1,33 +1,17 @@
-<<<<<<< HEAD
-import { useNavigate } from 'react-router-dom';
-import './App.css';
-=======
-import { Link, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import './App.css'
 
->>>>>>> 22497aab8e9d794f554f468ece5110cd1043fd1b
 function App() {
-  let navigate=useNavigate();
-  const getHelp=()=>{navigate(`/gethelp`)}
-  const giveHelp=()=>{navigate(`/givehelp`)}
-  const hangouts=()=>{navigate(`/hangouts`)}
+  let navigate = useNavigate();
+  const getHelp = () => { navigate(`/gethelp`) }
+  const giveHelp = () => { navigate(`/givehelp`) }
+  const hangouts = () => { navigate(`/hangouts`) }
   return (
-    <>
-<<<<<<< HEAD
     <div>
-        <button onClick={getHelp}>Tražim pomoć</button>
-        <button onClick={giveHelp}>Pružam pomoć</button>
-        <button onClick={hangouts}>Druženja</button>
+      <button onClick={getHelp}>Tražim pomoć</button>
+      <button onClick={giveHelp}>Pružam pomoć</button>
+      <button onClick={hangouts}>Druženja</button>
     </div>
-=======
-    <Link to="/emergencyNumbers"><button>Hitni brojevi</button> </Link>
-   <button>Hitne informacije</button>
-   <button>Tražim pomoć</button>
-   <button>Pruži brzu pomoć</button>
-   <button>Volontiranje</button>
-   <button>Druženje</button>
->>>>>>> 22497aab8e9d794f554f468ece5110cd1043fd1b
-    </>
   )
 }
 
--- DajRuku/src/pages/Get_help_page.jsx
+++ DajRuku/src/pages/Get_help_page.jsx
@@ -6,6 +6,14 @@ function Get_help() {
         <div>
             <button onClick={start}>Pocetna</button>
             <h1>Brojevi</h1>
+            <div>
+                <p>SOS: 112 </p>
+                <p>Policija: 192</p>
+                <p>Vatrogasci: 193</p>
+                <p>Hitna: 194</p>
+                <p>Služba traganja i spašavanja na moru: 195</p>
+                <p>HAK za pomoć na cesti: 1987</p>
+            </div>
             <div>pitanja dropdown</div>
         </div>
     )
